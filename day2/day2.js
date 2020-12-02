34c3fc0700253b1587535001b3584d7479c64331
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-02 22:07:17 +0000

    Day 2 part 2

--- day2/day2.js
+++ day2/day2.js
@@ -1,28 +1,42 @@
 const { loadInput } = require('../helpers');
 
 const parsePasswordEntry = (passwordEntry) => {
-    const matches = [...passwordEntry.matchAll(/^(\d+)-(\d+)\s(\w):\s(.+)$/)];
-    const [, min, max, letter, password] = matches[0];
-    return {
-        password,
-        policy: {
-            min, max, letter
-        }
-    };
+  const matches = [...passwordEntry.matchAll(/^(\d+)-(\d+)\s(\w):\s(.+)$/)];
+  const [, position1, position2, letter, password] = matches[0];
+  return {
+    password,
+    policy: {
+      position1: parseInt(position1, 10),
+      position2: parseInt(position2, 10),
+      letter,
+    },
+  };
 };
 
-const parsePasswordsList = (passwordsList) => passwordsList.map(entry => parsePasswordEntry(entry));
+const parsePasswordsList = (passwordsList) =>
+  passwordsList.map((entry) => parsePasswordEntry(entry));
 
-const isPasswordValid = (password, { min, max, letter }) => {
-    const letterCount = password.split(letter).length - 1;
-    return min <= letterCount && letterCount <= max;
+const isPasswordValidForRentalPlace = (password, { position1, position2, letter }) => {
+  const letterCount = password.split(letter).length - 1;
+  return position1 <= letterCount && letterCount <= position2;
+};
+
+const isPasswordValidForToboggan = (password, { position1, position2, letter }) => {
+  return (password[position1 - 1] === letter) !== (password[position2 - 1] === letter);
 };
 
 const start = () => {
-    const input = loadInput('day2/input');
-    const passwordEntries = parsePasswordsList(input);
-    const validPasswords = passwordEntries.filter(e => isPasswordValid(e.password, e.policy));
-    console.log(validPasswords.length);
+  const input = loadInput('day2/input');
+  const passwordEntries = parsePasswordsList(input);
+  const validPasswordsRentalPlace = passwordEntries.filter((e) =>
+    isPasswordValidForRentalPlace(e.password, e.policy)
+  );
+  console.log(`Rental place has ${validPasswordsRentalPlace.length} valid passwords`);
+
+  const validPasswordsToboggan = passwordEntries.filter((e) =>
+    isPasswordValidForToboggan(e.password, e.policy)
+  );
+  console.log(`Toboggan has ${validPasswordsToboggan.length} valid passwords`);
 };
 
 start();
--- helpers/index.js
+++ helpers/index.js
@@ -1,10 +1,7 @@
 const fs = require('fs');
 
-const loadInput = (path) =>
-  fs
-    .readFileSync(path, { encoding: 'utf8' })
-    .split('\n');
+const loadInput = (path) => fs.readFileSync(path, { encoding: 'utf8' }).split('\n');
 
 module.exports = {
-    loadInput,
+  loadInput,
 };
