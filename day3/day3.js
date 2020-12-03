d9dac0fed754186c12ac0af4c525419cdbd26577
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-03 18:38:14 +0000

    Day 3 part 2

--- day3/day3.js
+++ day3/day3.js
@@ -1,5 +1,13 @@
 const { loadInput } = require('../helpers');
 
+const slopes = [
+  [1, 1],
+  [3, 1],
+  [5, 1],
+  [7, 1],
+  [1, 2],
+];
+
 const generateTreeMatrix = (input) =>
   input.reduce((result, line) => {
     line.split('').forEach((point, i) => {
@@ -12,23 +20,25 @@ const generateTreeMatrix = (input) =>
 
 const hasArrived = (treeMatrix, { y }) => y > treeMatrix[0].length;
 
-const getNextPosition = ({ x, y }, patternSize) => ({
-  x: (x + 3) % patternSize,
-  y: y + 1,
+const getNextPosition = ({ x, y }, slope, patternSize) => ({
+  x: (x + slope[0]) % patternSize,
+  y: y + slope[1],
 });
 
-const followSlope = (treeMatrix, { x, y }, treesCount = 0) => {
+const followSlope = (treeMatrix, slope, { x, y }, treesCount = 0) => {
   if (hasArrived(treeMatrix, { y })) return treesCount;
   const newTreesCount = treeMatrix[x][y] ? treesCount + 1 : treesCount;
-  const nextPosition = getNextPosition({ x, y }, treeMatrix.length);
-  return followSlope(treeMatrix, nextPosition, newTreesCount);
+  const nextPosition = getNextPosition({ x, y }, slope, treeMatrix.length);
+  return followSlope(treeMatrix, slope, nextPosition, newTreesCount);
 };
 
 const start = () => {
   const input = loadInput('day3/input');
   const treeMatrix = generateTreeMatrix(input);
-  const treesCount = followSlope(treeMatrix, { x: 0, y: 0 });
-  console.log(`Number of trees: ${treesCount}`);
+
+  const treesCounts = slopes.map((slope) => followSlope(treeMatrix, slope, { x: 0, y: 0 }));
+  const result = treesCounts.reduce((acc, c) => acc * c, 1);
+  console.log(`Multiplying the number of trees encountered: ${result}`);
 };
 
 start();
