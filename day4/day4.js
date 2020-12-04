c79e1a3881ced70a90573dd456d1062dd15409e1
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-04 17:28:54 +0000

    Day 4 part 2

--- day4/day4.js
+++ day4/day4.js
@@ -1,11 +1,30 @@
 const { loadInput } = require('../helpers');
 
 const MANDATORY_PASSPORT_FIELDS = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
-// const OPTIONAL_PASSPORT_FIELDS = ['cid'];
+
+const validateHgt = (height) => {
+  const match = height.match(/^(\d+)(cm|in)$/);
+  if (!match) return false;
+
+  const heightValue = parseInt(match[1], 10);
+  return match[2] === 'cm'
+    ? heightValue >= 150 && heightValue <= 193
+    : heightValue >= 59 && heightValue <= 76;
+};
+
+const PASSPORT_FIELDS_VALIDATORS = {
+  byr: (v) => v >= 1920 && v <= 2002,
+  iyr: (v) => v >= 2010 && v <= 2020,
+  eyr: (v) => v >= 2020 && v <= 2030,
+  hgt: (v) => validateHgt(v),
+  hcl: (v) => /^#[a-f0-9]{6}$/.test(v),
+  ecl: (v) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(v),
+  pid: (v) => /^\d{9}$/.test(v),
+};
 
 const isPassportValid = (passport) =>
   MANDATORY_PASSPORT_FIELDS.reduce((isValid, field) => {
-    if (!passport[field]) return false;
+    if (!passport[field] || !PASSPORT_FIELDS_VALIDATORS[field](passport[field])) return false;
     return isValid;
   }, true);
 
