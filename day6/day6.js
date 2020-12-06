6225002fdc30e85f074c6f7ef472d145baa7504e
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-06 14:18:48 +0000

    Day 6 part 2

--- day6/day6.js
+++ day6/day6.js
@@ -1,19 +1,27 @@
 const { loadInput } = require('../helpers');
 
-const countCombinedYesAnswers = (answers) => [...new Set(answers.split(''))].length;
+const countCombinedYesAnswers = (answersArray) => {
+  const result = answersArray.reduce((intersection, answers) => {
+    return intersection.filter((answer) => answers.includes(answer));
+  }, answersArray[0]);
+  return result.length;
+};
 
-const getGroupsYesAnswersCount = (input, result = [], index = 0, currentGroupAnswers = '') => {
+const getGroupsYesAnswersCount = (input, result = [], index = 0, currentGroupAnswers = []) => {
   if (index === input.length) return result;
   if (input[index] === '') {
     return getGroupsYesAnswersCount(
       input,
       [...result, countCombinedYesAnswers(currentGroupAnswers)],
       index + 1,
-      ''
+      []
     );
   }
 
-  return getGroupsYesAnswersCount(input, result, index + 1, currentGroupAnswers + input[index]);
+  return getGroupsYesAnswersCount(input, result, index + 1, [
+    ...currentGroupAnswers,
+    input[index].split(''),
+  ]);
 };
 
 const start = () => {
