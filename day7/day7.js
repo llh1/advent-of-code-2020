b0af9a3a4540980a049cd3e59facd6b28c34b4f5
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-07 17:08:56 +0000

    Day 7 part 2

--- day7/day7.js
+++ day7/day7.js
@@ -5,7 +5,7 @@ const SHINY_GOLD = 'shiny gold';
 const parseBagRuleContent = (content) => {
   const matches = [...content.matchAll(/(\d+)\s([\w\s]+)\sbags?/g)];
   return matches.map((match) => ({
-    count: match[1],
+    count: parseInt(match[1], 10),
     color: match[2],
   }));
 };
@@ -30,11 +30,22 @@ const findContainersOf = (bagColor, bagRules, result = []) => {
   }, result);
 };
 
+const findIndividualBagsCountInside = (bagColor, bagRules) => {
+  if (bagRules[bagColor].length === 0) return 0;
+  return bagRules[bagColor].reduce((acc, rule) => {
+    return acc + rule.count + rule.count * findIndividualBagsCountInside(rule.color, bagRules);
+  }, 0);
+};
+
 const start = () => {
   const input = loadInput('day7/input');
   const bagRules = parseBagRules(input);
+
   const containers = findContainersOf(SHINY_GOLD, bagRules);
   console.log(`${containers.length} bags can contain a ${SHINY_GOLD} bag.`);
+
+  const individualBags = findIndividualBagsCountInside(SHINY_GOLD, bagRules);
+  console.log(`A single ${SHINY_GOLD} bag must contain ${individualBags} other bags.`);
 };
 
 start();
