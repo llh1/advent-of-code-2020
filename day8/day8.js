9ff143493f6ac72c769bef8f1e10c6c02be1ca7f
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-08 11:02:54 +0000

    Day 8

--- day8/day8.js
+++ day8/day8.js
@@ -20,25 +20,55 @@ const runInstructions = (
   instructionIndex = 0,
   executedIndex = []
 ) => {
-  if (executedIndex.includes(instructionIndex)) return accumulator;
+  if (instructions.length <= instructionIndex) {
+    return { status: 'COMPLETE', accumulator };
+  }
+
+  if (executedIndex.includes(instructionIndex)) {
+    return { status: 'ERROR', accumulator };
+  }
 
   const [newAccumulator, nextInstructionIndex] = execute(
     instructions,
     instructionIndex,
     accumulator
   );
+
   return runInstructions(instructions, newAccumulator, nextInstructionIndex, [
     ...executedIndex,
     instructionIndex,
   ]);
 };
 
+const replaceNextNopJmp = (fromIndex, instructions) => {
+  const remainingChangeableInstructions = instructions.slice(fromIndex);
+  const nextNopJmpIndex = remainingChangeableInstructions.findIndex((i) => /nop|jmp/.test(i));
+  const newInstructions = [...instructions];
+  newInstructions[nextNopJmpIndex + fromIndex] = remainingChangeableInstructions[
+    nextNopJmpIndex
+  ].startsWith('nop')
+    ? remainingChangeableInstructions[nextNopJmpIndex].replace('nop', 'jmp')
+    : remainingChangeableInstructions[nextNopJmpIndex].replace('jmp', 'nop');
+  return newInstructions;
+};
+
 const start = () => {
   const input = loadInput('day8/input');
-  const accumulatorValue = runInstructions(input);
-  console.log(
-    `Immediately before the program would run an instruction a second time, the value in the accumulator is ${accumulatorValue}.`
-  );
+  const instructionsCount = input.length;
+  let instructions = input;
+
+  for (let i = 0; i < instructionsCount; i++) {
+    const result = runInstructions(instructions);
+    if (result.status === 'ERROR') {
+      instructions = replaceNextNopJmp(i, input);
+      console.log(instructions);
+    } else {
+      console.log(
+        `Immediately before the program would run an instruction a second time, the value in the accumulator is ${result.accumulator}.`
+      );
+      return;
+    }
+  }
 };
 
 start();
