c09947313a36fa175919b8ac6e8dd3c16e8a844b
Loic Le Henaff <loic.lehenaff@bbc.co.uk>
2020-12-09 22:26:00 +0000

    Day 9 part 2

--- day9/day9.js
+++ day9/day9.js
@@ -11,16 +11,38 @@ const isSumOf2PreambleNumbers = (number, lastNumbers) => {
   return false;
 };
 
+const findContiguousSetWhichSumTo = (weaknessNumber, numberList) => {
+  let contiguousSet = [];
+  for (let i = 0; i < numberList.length; i++) {
+    contiguousSet.push(numberList[i]);
+    for (let j = i + 1; j < numberList.length; j++) {
+      contiguousSet.push(numberList[j]);
+      const sum = contiguousSet.reduce((acc, n) => acc + n, 0);
+      if (sum === weaknessNumber) {
+        return contiguousSet;
+      }
+      if (sum >= weaknessNumber) {
+        contiguousSet = [];
+        break;
+      }
+    }
+  }
+};
+
 const start = () => {
   const input = loadInput('day9/input');
   const numbers = input.map((i) => parseInt(i, 10));
-
+  let weaknessNumber;
   for (let i = 0; i < numbers.length - PREAMBLE_SIZE; i++) {
     if (!isSumOf2PreambleNumbers(numbers[i + PREAMBLE_SIZE], numbers.slice(i, PREAMBLE_SIZE + i))) {
-      console.log(numbers[i + PREAMBLE_SIZE]);
-      return;
+      weaknessNumber = numbers[i + PREAMBLE_SIZE];
+      break;
     }
   }
+
+  const contiguousSet = findContiguousSetWhichSumTo(weaknessNumber, numbers);
+  const encryptionWeakness = Math.min(...contiguousSet) + Math.max(...contiguousSet);
+  console.log(`Encryption weakness: ${encryptionWeakness}`);
 };
 
 start();
